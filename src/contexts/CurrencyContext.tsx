27b04985bb797327d37301e5556c7424412a572f
Luiz Otávio <luizotavio0106@outlook.com>
2023-08-15 17:41:41 -0300

    reafactor: created type for payment method

--- src/contexts/CurrencyContext.tsx
+++ src/contexts/CurrencyContext.tsx
@@ -1,12 +1,13 @@
 import { PropsWithChildren, createContext, useState } from 'react';
+import { PaymentType } from '../types/payment-type';
 
 type ICurrencyContext = {
   value: string;
   rate: string;
-  paymentType: string;
+  paymentType: PaymentType;
   setValue: React.Dispatch<React.SetStateAction<string>>;
   setRate: React.Dispatch<React.SetStateAction<string>>;
-  setPaymentType: React.Dispatch<React.SetStateAction<string>>;
+  setPaymentType: React.Dispatch<React.SetStateAction<PaymentType>>;
 };
 
 export const CurrencyContext = createContext<ICurrencyContext | null>(null);
@@ -14,7 +15,9 @@ export const CurrencyContext = createContext<ICurrencyContext | null>(null);
 export const CurrencyProvider = ({ children }: PropsWithChildren) => {
   const [value, setValue] = useState('');
   const [rate, setRate] = useState('');
-  const [paymentType, setPaymentType] = useState('');
+  const [paymentType, setPaymentType] = useState<PaymentType>(
+    PaymentType.MONEY
+  );
 
   return (
     <CurrencyContext.Provider
---
+++ src/types/payment-type.ts
@@ -0,0 +1,4 @@
+export enum PaymentType {
+  'MONEY',
+  'CARD',
+}
